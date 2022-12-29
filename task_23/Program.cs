35dbb68cb58f08432831bbc197e5dee0211b9611
Konstantin <ksemenec@mail.ru>
2022-12-29 14:05:46 +0500

    задача 23

---
+++ task_23/Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 23
+Напишите программу, которая принимает на вход число (N)
+ и выдаёт таблицу кубов чисел от 1 до N.*/
+Console.WriteLine("Введите положительное число  ");
+int number = Convert.ToInt32(Console.ReadLine());
+int n = 1;
+if (number <= 0)
+{
+Console.WriteLine("Введенное число отрицательное");
+return;
+}
+while(n <= number)
+{
+  double res = Math.Pow(n, 3);
+  Console.WriteLine($"{res}");
+  n ++;
+}
